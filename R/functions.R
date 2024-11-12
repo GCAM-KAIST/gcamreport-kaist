acd54e001efdf9d3c913d0fc4388250e398b9c42
klau506 <claudia.rodes@bc3research.org>
2024-11-12 10:59:05 +0100

    ComDef: production price

--- R/functions.R
+++ R/functions.R
@@ -3112,6 +3112,37 @@ get_energy_price <- function(GCAM_version = "v7.0") {
   energy_price_clean <<- energy_price_clean
 }
 
+
+#' get_production_price
+#'
+#' Compute industry production prices
+#' @keywords internal price process
+#' @return `production_price_clean` global variable
+#' @importFrom magrittr %>%
+#' @export
+get_production_price <- function(GCAM_version = "v7.0") {
+  production_price_clean <- NULL
+
+  production_price_clean <- rbind(
+    rgcam::getQuery(prj, "iron and steel prices"),
+    rgcam::getQuery(prj, "chemical prices"),
+    rgcam::getQuery(prj, "aluminum prices")
+  ) %>%
+    left_join_strict(filter_variables(get(paste('production_price_map',GCAM_version,sep='_'), envir = asNamespace("gcamreport")), "production_price_clean"),
+                     by = 'sector') %>%
+    # $/kg = 1e6 $/Mt
+    dplyr::mutate(value = dplyr::if_else(Units == '1975$/kg', value * 1e6, value)) %>%
+    # 18.6 $/GJ ammonia = 1 $/Mt ammonia
+    dplyr::mutate(value = dplyr::if_else(Units == '1975$/GJ', value * 18.6, value)) %>%
+    # 1975$ to 2010$
+    dplyr::mutate(value = value *
+                    get(paste('convert',GCAM_version,sep='_'), envir = asNamespace("gcamreport"))[['conv_75USD_10USD']]) %>%
+    dplyr::select(dplyr::all_of(gcamreport::long_columns))
+
+  production_price_clean <<- production_price_clean
+}
+
+
 # Capacity and investment
 # ==============================================================================================
 # electricity capacity
