d8c0ef1e555a4a50c8669dca0a60f1403883f0ea
Andy M <andy.miller331@gmail.com>
2025-02-17 21:00:16 -0500

    Allow longer final warning - CO2 markets

--- R/functions.R
+++ R/functions.R
@@ -3565,12 +3565,12 @@ get_energy_price_tmp <- function(GCAM_version = "v7.1") {
   }
   missing_markets <- setdiff(unique(tmp1$market), c(unique(CO2_market_filteredReg$market),NA))
   if (interactive() & length(missing_markets) != 0) {
-    warning(sprintf('ATTENTION: CO2 markets including:\n %.100s \nare not present in the `co2_market_new` mapping file.',
+    warning(sprintf('ATTENTION: CO2 markets including:\n %.800s \nare not present in the `co2_market_new` mapping file.',
                     paste(missing_markets, collapse = ", ")))
 
     # user response
     user_input <- readline(prompt =
-                             sprintf('CO2 markets including:\n %.100s \nare not present in the `co2_market_new` mapping file.\nDo you want to continue without adding them (Y/N)? Press Y or N: ',
+                             sprintf('ATTENTION: CO2 markets including:\n %.100s \nare not present in the `co2_market_new` mapping file.\nDo you want to continue without adding them (Y/N)? Press Y or N: ',
                                      paste(missing_markets, collapse = ", ")))
 
     # handling user response
