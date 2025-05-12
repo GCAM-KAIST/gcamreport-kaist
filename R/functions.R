5f0a868b08bf6dd1ab3fb95bd1c5b5eeabcd9b42
klau506 <claudia.rodes@bc3research.org>
2025-05-12 15:19:34 +0200

    ComDef: fix listYears function

--- R/functions.R
+++ R/functions.R
@@ -93,7 +93,7 @@ listYears <- function (projData, scenarios = NULL, queries = NULL, anyscen = TRU
     all_years <- unlist(sqlist)
     all_years <- all_years[!is.na(all_years)]
     year_counts <- table(all_years)
-    result <- sort(as.numeric(names(year_counts[year_counts > 10])))
+    result <- sort(as.numeric(names(year_counts[year_counts > length(queries)/2])))
   } else {
     # Intersect case: just intersect all elements
     result <- Reduce(intersect, Reduce(intersect, sqlist))
