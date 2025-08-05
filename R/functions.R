4f39c72972e0249f0f010c942ad3007b2f477530
klau506 <claudia.rodes@bc3research.org>
2025-08-05 12:19:58 +0200

    ComDef: issue 2-3 - fix ag trade units

--- R/functions.R
+++ R/functions.R
@@ -1635,7 +1635,7 @@ get_ag_trade <- function(GCAM_version = "v7.1") {
     dplyr::summarise(value = sum(value)) %>%
     dplyr::ungroup() %>%
     # billion m3 to million m3 for Trade|Forestry
-    dplyr::mutate(value = dplyr::if_else(grepl("Trade|Forestry", var),
+    dplyr::mutate(value = dplyr::if_else(grepl("Trade\\|Forestry", var),
                                          value / get(paste('convert',GCAM_version,sep='_'), envir = asNamespace("gcamreport"))[['conv_million_billion']],
                                          value)) %>%
     dplyr::select(dplyr::all_of(gcamreport::long_columns))
@@ -1665,7 +1665,7 @@ get_ag_trade <- function(GCAM_version = "v7.1") {
     dplyr::summarise(value = sum(value)) %>%
     dplyr::ungroup() %>%
     # billion m3 to million m3 for Trade|Forestry
-    dplyr::mutate(value = dplyr::if_else(grepl("Trade|Forestry", var),
+    dplyr::mutate(value = dplyr::if_else(grepl("Trade\\|Forestry", var),
                                          value / get(paste('convert',GCAM_version,sep='_'), envir = asNamespace("gcamreport"))[['conv_million_billion']],
                                          value)) %>%
     dplyr::select(dplyr::all_of(gcamreport::long_columns))
