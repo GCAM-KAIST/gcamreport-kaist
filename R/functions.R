eb5124a26e040947230ccf02d16c5fe1f0f63fde
klau506 <claudia.rodes@bc3research.org>
2026-05-20 16:06:29 +0200

    Homogenize code

--- R/functions.R
+++ R/functions.R
@@ -5985,14 +5985,14 @@ get_total_investment <- function(GCAM_version = "v7.1") {
 
   # Remove aggregated value from invest tibbles,
   # now that summed value is in total_investment_clean
-  resource_investment_clean <<- resource_investment_clean |>
-      dplyr::filter(var != 'Investment|Energy Supply')
-
-  elec_investment_clean <<- elec_investment_clean |>
+  resource_investment_clean <<- resource_investment_clean %>%
     dplyr::filter(var != 'Investment|Energy Supply')
 
-  nonelec_investment_clean <<- nonelec_investment_clean |>
-  dplyr::filter(var != 'Investment|Energy Supply')
+  elec_investment_clean <<- elec_investment_clean %>%
+    dplyr::filter(var != 'Investment|Energy Supply')
+
+  nonelec_investment_clean <<- nonelec_investment_clean %>%
+    dplyr::filter(var != 'Investment|Energy Supply')
 
 
   total_investment_clean <<- total_investment_clean
